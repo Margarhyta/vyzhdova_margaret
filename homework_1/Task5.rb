bd89b2f8f02523c7e6922f62beae14db038137e0
Margarhyta <m.vyzhdova04@gmail.com>
2021-11-02 21:57:50 +0200

    Change Task 5

--- homework_1/Task5.rb
+++ homework_1/Task5.rb
@@ -4,6 +4,6 @@ hash = {
    'kEy' => :value_1,
    :key  => :value_2,
     Key:    :value_3
-   }
+}
    
-   p hash.count { |k,_| k.to_s.downcase k == 'key' }
+   p hash.count { |k,_| k.to_s.downcase == 'key' }
