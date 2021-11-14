0a00d0138d6f7d107af7f97dcc937605eeed8a0f
Margarhyta <m.vyzhdova04@gmail.com>
2021-11-14 20:55:37 +0200

    change task1

--- homework_2/task2.rb
+++ homework_2/task2.rb
@@ -1,14 +1,7 @@
-
 loop do
-    puts 'Hi, please enter your number'
-    answer = gets.chomp
-    counter = 0
-    answer.each_char do |n|
-    n == "1"
-    counter += 1 if n == "1"
-    end
-    break if counter > 1
-    p answer.to_i * 256
-    end
-    puts 'Умови недотримані, зупиняю виконання програми'
-    
+  puts 'Hi, please enter your number'
+  answer = gets.chomp
+  break if answer.count('1') > 1
+  p answer.to_i * 256
+end
+puts 'Умови недотримані, зупиняю виконання програми'
